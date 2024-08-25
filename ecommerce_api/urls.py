e03f21a9791cd4e4aad94df707194f709c1eb520
onise2001 <onise.babulashvili@yahoo.com>
2024-08-26 00:04:12 +0400

    Added Subscription and FAQ urls

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet, OrderViewSet, CartViewSet, CartItemViewSet
+from .views import ProductViewSet, CategoryViewSet, OrderViewSet, CartViewSet, CartItemViewSet, SubscriptionViewSet, FAQViewSet
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'product', ProductViewSet)
@@ -8,6 +8,9 @@ router.register(r'category', CategoryViewSet)
 router.register(r'order', OrderViewSet)
 router.register(r'cart', CartViewSet, basename='cart')
 router.register(r'cart-item', CartItemViewSet)
+router.register(r'subscription', SubscriptionViewSet)
+router.register(r'faq', FAQViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
