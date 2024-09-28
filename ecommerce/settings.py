349cb0e39064168b81bdb8419902cd8a7374d015
onise2001 <onise.babulashvili@yahoo.com>
2024-09-28 14:23:25 +0400

    Changed database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -94,7 +94,21 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DB_URL'))
+    #'default': dj_database_url.parse(os.getenv('DB_URL')),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+
+#    'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+
 }
 
 
