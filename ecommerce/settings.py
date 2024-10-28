1c0919e6d011fc564a7fe0ae7819527e8a0ee1f4
onise2001 <onise.babulashvili@yahoo.com>
2024-10-28 18:41:33 +0400

    added a new server to nginx.conf

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -171,9 +171,10 @@ AWS_S3_REGION_NAME =  os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
